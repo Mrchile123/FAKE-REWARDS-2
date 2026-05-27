952e9995bd2a8dd68e5a6a8fb3b31ddfc52dd27a
emirontop1 <dincemr@gmail.com>
2026-05-27 15:33:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
-#include <Geode/Bindings.hpp>
+#include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include "EmirGui.hpp" // Kütüphanemizi çağırdık
 
 using namespace geode::prelude;
 
-class $modify(MenuLayer) {
-	void onMoreGames(CCObject*) {
-		FLAlertLayer::create(
-			"Geode",
-			"Hello World from my Custom Mod!",
-			"OK"
-		)->show();
-	}
+class $modify(MyMenuLayer, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        // Ana menüdeki alt menüyü bulalım
+        auto menu = this->getChildByID("bottom-menu");
+
+        // Kütüphanemizden butonu oluşturalım
+        auto btn = EmirGui::createCircleBtn(
+            "GJ_likeBtn_001.png", 
+            this, 
+            menu_selector(MyMenuLayer::onEmirClick)
+        );
+
+        // Geode özelliklerini tanımlayalım
+        btn->setID("emir-buton"_spr);
+        menu->addChild(btn);
+        
+        // Menüyü otomatik hizala (Uğraşmanı engeller)
+        menu->updateLayout();
+
+        return true;
+    }
+
+    void onEmirClick(CCObject* sender) {
+        FLAlertLayer::create(
+            "Başarılı!", 
+            "EmirGui kütüphanesi ve yeni dosya yapısı çalışıyor.", 
+            "Tamam"
+        )->show();
+    }
 };
