b54825ad3c7d56b5c9a2564b561ef705fbcc69b7
emirontop1 <dincemr@gmail.com>
2026-05-27 17:38:47 +0300

    Update EmirGui.hpp

--- src/EmirGui.hpp
+++ src/EmirGui.hpp
@@ -4,23 +4,37 @@
 using namespace geode::prelude;
 
 namespace EmirGui {
-    // Circle Button oluşturucu
-    inline CCMenuItemSpriteExtra* createCircleBtn(const char* sprName, CCObject* target, SEL_MenuHandler callback) {
-        auto spr = CircleButtonSprite::createWithSpriteFrameName(sprName);
-        return CCMenuItemSpriteExtra::create(spr, target, callback);
-    }
+    // Sürükle-bırak panel
+    class EmirMenu : public Popup<std::string> {
+    protected:
+        bool setup(std::string title) override {
+            this->setTitle(title);
+            auto menu = CCMenu::create();
+            menu->setPosition({0, 0});
+            this->m_mainLayer->addChild(menu);
 
-    // Text Button oluşturucu
-    inline CCMenuItemSpriteExtra* createTextBtn(const char* text, CCObject* target, SEL_MenuHandler callback) {
-        auto spr = ButtonSprite::create(text);
-        return CCMenuItemSpriteExtra::create(spr, target, callback);
-    }
+            // Noclip Toggle
+            auto noclip = CCMenuItemToggler::createWithStandardSprites(this, menu_selector(EmirMenu::onToggle), false);
+            noclip->setPosition({100, 150});
+            menu->addChild(noclip);
 
-    // Menüye güvenli ekleme ve hizalama (Layout)
-    inline void addToMenu(CCMenu* menu, CCNode* node) {
-        if (menu && node) {
-            menu->addChild(node);
-            menu->updateLayout();
+            // ESP Toggle
+            auto esp = CCMenuItemToggler::createWithStandardSprites(this, menu_selector(EmirMenu::onToggle), false);
+            esp->setPosition({100, 100});
+            menu->addChild(esp);
+            
+            return true;
         }
-    }
+        void onToggle(CCObject* sender) { /* Toggle mantığı */ }
+    public:
+        static EmirMenu* create() {
+            auto ret = new EmirMenu();
+            if (ret && ret->initAnchored(300, 200, "Emir Menu")) {
+                ret->autorelease();
+                return ret;
+            }
+            CC_SAFE_DELETE(ret);
+            return nullptr;
+        }
+    };
 }
