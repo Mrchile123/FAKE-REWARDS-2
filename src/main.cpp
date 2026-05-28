f114c9fa62a56d307f931349446e9cc5e1aff013
emirontop1 <dincemr@gmail.com>
2026-05-28 20:20:40 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,32 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-
+#include <Geode/ui/GeodeUI.hpp>
 using namespace geode::prelude;
-
-class FloatingMenu : public cocos2d::CCLayer {
+class ModernMenu : public CCLayer {
 protected:
-    cocos2d::CCLayer* m_mainGui;
+    CCMenu* m_buttonMenu = nullptr;
+    CCMenuItemSpriteExtra* m_floatButton = nullptr;
+    CCLayerColor* m_panel = nullptr;
+    CCScale9Sprite* m_background = nullptr;
+    bool m_open = false;
     bool m_dragging = false;
-
+    CCPoint m_dragOffset;
+    float m_savedX = 80.f;
+    float m_savedY = 160.f;
+public:
+    static ModernMenu* create() {
+        auto ret = new ModernMenu();
+        if (ret && ret->init()) {
+            ret->autorelease();
+            return ret;
+        }
+        delete ret;
+        return nullptr;
+    }
     bool init() override {
-        // 1. Ana GUI (Gizli başlar)
-        m_mainGui = CCLayer::create();
-        m_mainGui->setVisible(false);
-        this->addChild(m_mainGui, 10);
-
-        // 2. Sürüklenen Daire Buton
-        auto circleSprite = CCSprite::create("circle_button.png"); // Kendi daire görselini ekle
-        auto toggleBtn = CCMenuItemSprite::create(circleSprite, circleSprite, this, menu_selector(FloatingMenu::onToggle));
-        auto menu = CCMenu::create(toggleBtn, nullptr);
-        menu->setPosition({50, 50});
-        this->addChild(menu);
-
-        // Dokunmatik olaylarını dinle
+        if (!CCLayer::init())
+            return false;
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
         this->setTouchEnabled(true);
-        return true;
-    }
-
-    void onToggle(CCObject*) {
-        m_mainGui->setVisible(!m_mainGui->isVisible());
-    }
-
-    // Sürükleme Mantığı
-    void ccTouchMoved(cocos2d::CCTouch* pTouch, cocos2d::CCEvent* pEvent) override {
-        auto pos = pTouch->getLocation();
-        this->setPosition(pos); // Daireyi sürükle
-    }
-};
-
-// Hem Menu hem PlayLayer'da çalışması için
-class $modify(MyPlayLayer, PlayLayer) {
-    bool init() override {
-        if (!PlayLayer::init()) return false;
-        this->addChild(FloatingMenu::create(), 100);
-        return true;
-    }
-};
+        this->setTouchMode(kCCTouchesOneByOne);
+        this->setKeypadEnabled(true);
